011ea04da0ec9807780016c6e7b277474ee666e1
Denis <presetslr@gmail.com>
2019-09-24 01:50:16 +0300

    fix: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function zeros(expression) {
-  // your solution
+    function factorial(num) {
+        return num ? num * factorial(num - BigInt(1)) : BigInt(1);
+    }
+
+    function evenFactorial(num) {
+        return num > BigInt(1) ? num * evenFactorial(num - BigInt(2)) : BigInt(1);
+    }
+
+    let res = BigInt(1);
+    let zeros = 0;
+
+    expression.split('*').map(
+        a => (/^\d+[!]{2}$/.test(a)) ? evenFactorial(BigInt(parseInt(a))) : factorial(BigInt(parseInt(a)))).forEach(
+        multiply => res *= BigInt(multiply));
+    res.toString().split("").reverse().some(digit => {
+        if (digit == 0) {
+            zeros++;
+        } else {
+            return true;
+        }
+    });
+    return zeros;
 }
